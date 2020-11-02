401e87062cd2aa6ff66dc4133555fc1c91ca37e2
orontamir <orontamir@gmail.com>
2020-11-02 18:13:53 +0200

    Add application user

---
+++ Garages_Recruit_1271/Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Garages_Recruit_1271.Models
+{
+    public class ApplicationUser
+    {
+    }
+}
---
+++ Garages_Recruit_1271/Utilities/ValidationEmailDomainAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Garages_Recruit_1271.Utilities
+{
+    public class ValidationEmailDomainAttribute: ValidationAttribute
+    {
+        public ValidationEmailDomainAttribute(string allowedDomain)
+        {
+
+        }
+        public override bool IsValid(object value)
+        {
+            return base.IsValid(value);
+        }
+    }
+}
